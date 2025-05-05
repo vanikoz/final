3b560f129aae25a5d2745d9c692b234f4ac5a7e6
vanikoz <navioamargareth@gmail.com>
2025-05-06 01:43:24 +0800

    Add files via upload

---
+++ final/script.js
@@ -0,0 +1,21 @@
+let list = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let dots = document.querySelectorAll('.slider .dots li');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+let active = 0;
+let lengthItems = items.length - 1;
+
+next.onclick = function(){
+    if(active + 1 > lengthItems){
+        active = 0;
+    }else{
+        active = active + 1;
+    }
+    reloadSlider();
+}
+function reloadSlider(){
+    let checkLeft = items[active].offsetLeft;
+    list.style.left = -checkLeft + 'px';
+}
